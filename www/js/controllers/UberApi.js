20e50d770af4b5a133cf66a6d5b13965698aa750
omart075 <omart075@fiu.edu>
2016-09-24 03:14:19 -0400

    Uber api controller intergrated

--- www/js/controllers/UberApi.js
+++ www/js/controllers/UberApi.js
@@ -2,9 +2,19 @@
 
 // authorization
 
-angular.module('app').controller('UberCtrl', [$scope, $http, function ($scope, $http) {
-    
-    var test = function(startLat, startLng, finLat, finLng) = {
+var auth = 'Token Xqf0ZY_v87Xz-T8_ihk22lzReP2qSm0VLtib9QW6';
 
+angular.module('app').controller('UberCtrl', ['$scope', '$http', function ($scope, $http) {
+
+    $scope.uber= function(startLat, startLng, finLat, finLng) {
+	$http({
+	    method: "GET",
+	    url: 'https://api.uber.com/v1/estimates/price?start_latitude=' + startLat + '&start_longitude=' + startLng + '&end_latitude=' + finLat + '&end_longitude=' + finLng,
+	    headers: {
+		'Authorization': auth
+	    }
+	}).success(function (res, status, headers) {
+	    console.log(res);
+	});
     };
 }]);
