38f1bc991dff5c1f2dc714bbee8c477dc9739474
Adam Tahoun <ataho001@fiu.edu>
2016-09-24 01:33:26 -0400

    search and input box...no autocomplete yet...hannn

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('app', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,3 +22,28 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+
+app.directive('googleplace', function() {
+    return {
+	require: 'ngModel',
+	link: function(scope, element, attrs, model) {
+	    var options = {
+		types: [],
+		componentRestrictions: {}
+	    };
+	    scope.gPlace = new google.maps.places.Autocomplete(element[0], options);
+
+	    google.maps.event.addListener(scope.gPlace, 'place_changed', function() {
+		scope.$apply(function() {
+		    model.$setViewValue(element.val());
+		});
+	    });
+	}
+    };
+});
+//myApp.factory('myService', function() {});
+
+function MyCtrl($scope) {
+    $scope.gPlace;
+}
